a7ab543732b3e449f6b2e053d280915adeb1c15d
bellenker <libertybelle0524.er@gmail.com>
2024-01-20 18:02:56 -0700

    updates to java script w2

---
+++ scripts/w02-task.js
@@ -0,0 +1,32 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Elizabeth Wilhite';
+const Year = new Date();
+let currentYear = Year.getFullYear();
+let profilePicture = ("images/personalimage.jpg");
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
+
+/* Step 4 - Adding Content */
+imageElement.setAttribute('src', profilePicture);
+nameElement.innerHTML = (`<strong>${fullName}</strong>`);
+yearElement.innerHTML = (`${currentYear}`);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+/* Step 5 - Array */
+let food = ["alfredo", "loaded baked potato", "lasagna" , "ice cream" , "beef stew on rice"];
+foodElement.innerHTML += `<br>${food}`;
+food.shift();
+foodElement.innerHTML += `<br>${food}`;
+food.pop();
+foodElement.innerHTML += `<br>${food}`;
+
+
+

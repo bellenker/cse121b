796f76752f9cc2ddf85fe063517462c000234cf0
bellenker <libertybelle0524.er@gmail.com>
2024-02-10 11:06:09 -0700

    Got the filter to display, but not filter

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -5,7 +5,7 @@ const templesElement=document.getElementById('temples');
 let templeList = [];
 
 /* async displayTemples Function */
-const displayTemples=(temples) =>{
+const displayTemples = (temples) =>{
     temples.forEach (temple =>{
         let article = document.createElement('article');
         let h3 = document.createElement('h3');
@@ -29,31 +29,31 @@ const getTemples = async() =>{
 };
 
 /* reset Function */
-const reset = ()=>{
-    templesElement.innerHTML="";
+const reset = () => {
+    templesElement.innerHTML='';
 };
 
 /* filterTemples Function */
-const filterTemples = (temples) =>{
+const filterTemples = (temples) => {
     reset();
     let filter = document.getElementById('filtered').value;
     switch (filter) {
         case "utah":
             temples.filter(temple => temple.location.includes("Utah"));
-            break;
-        case "nonutah":
-            temples.filter(temple => temple.location.indexOf("Utah") === -1);
+            return temple.utah;
+        case "notutah":
+            temples.filter(temple => {return temple.location.indexOf("Utah") === -1});
             break;
         case "older":
-            temples.filter(temple => temple.dedicated > new Date(1950, 0, 1));
+            temples.filter(temple => {return temple.dedicated > new Date(1950, 0, 1)});
             break;
         default:
-            temples;
+            return temples;
     }
-    return temples;
 };
 
-getTemples();
 
 /* Event Listener */
-document.querySelector('#filtered').addEventListener('change', () => {filterTemples(templeList)});
+document.querySelector('#filtered').addEventListener('change', () => {filterTemples(filter)});
+
+getTemples();
